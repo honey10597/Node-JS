ad939c46335bf2048b7b2d5e75c6b3b3d339b557
Taejesh <taejesh.saini@codebrewinnovations.com>
2023-10-27 19:58:50 +0530

    fix

---
+++ httpserver.js
@@ -0,0 +1,16 @@
+const http = require("http")
+
+const port = process.env.PORT || 3001
+
+const server = http.createServer((req, res) => {
+
+    console.log(req)
+
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html")
+    res.end("<h1>This is my first server</h1> <p> chak deeee </p>")
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on post ${port}`)
+})
